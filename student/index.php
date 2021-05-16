7394c1603cfc58b5a87f0f0bfe0f5198a95dbff2
jacktrinh123 <hoangtq.iuh@gmail.com>
2021-05-16 10:49:30 +0700

    rename head to headlibs fix headlibs

--- student/index.php
+++ student/index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include("./partials/head.php") ?>
-
-
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <?php include("./partials/headlibs.php")?>
+</head>
 <body>
   <?php include("./partials/header.php")?>
-  <div class="content">
-
-  </div>
-
+  <div class="content"></div>
   <?php include("./partials/footer.php")?>
 </body>
-
 </html>
