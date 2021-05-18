cbdaeee5bc165d2ce998827f21f43e458f212f89
Nguyen Tran My Hien <84259183+nguyentranmyhien@users.noreply.github.com>
2021-05-18 13:34:28 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -3,11 +3,8 @@
 <head>
         
         <meta charset="utf-8">
-        <title>Dashboard | Skote - Responsive Bootstrap 4 Admin Dashboard</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description">
-        <meta content="Themesbrand" name="author">
-        <!-- App favicon -->
+        
+        
         <link rel="shortcut icon" href="assets\images\logo-lightt.png">
 
         <!-- Bootstrap Css -->
