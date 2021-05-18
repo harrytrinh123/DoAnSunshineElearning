c0749e183567d083a4f57a43e58d14b18b0f3642
nvt2k <thuannguyencapricorn@gmail.com>
2021-05-18 08:58:09 +0700

    add,update

--- student/Forum.php
+++ student/Forum.php
@@ -6,12 +6,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Forum</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
-  <script src="https://kit.fontawesome.com/4197e7d2f0.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="assets/css/forum.css" />
+    <link rel="stylesheet" href="./assets/css/index.css">
 
 </head>
 <body>
@@ -21,7 +17,7 @@
             <div class="col-8 col-s-8 menu">
                 <div id="topic_name" class="text-uppercase font-weight-bold">Topic Name</div><br />
                 <div class="list-group">
-                    <a href="Postdetal.php" class="list-group-item list-group-item-action"><img src="images/user_avatar2.png" width="40" height="40" /> jkabfaksf</a>
+                    <a href="Postdetail.php" class="list-group-item list-group-item-action"><img src="images/user_avatar2.png" width="40" height="40" /> jkabfaksf</a>
 					<a href="Postdetail.php" class="list-group-item list-group-item-action"><img src="images/user_avatar3.png" width="40" height="40" /> Dapibus ac facilisis in</a>
 					<a href="Postdetail.php" class="list-group-item list-group-item-action"><img src="images/user_avatar2.png" width="40" height="40" /> jkabfaksf</a>
 					<a href="Postdetail.php" class="list-group-item list-group-item-action"><img src="images/user_avatar.png" width="40" height="40" /> Dapibus ac facilisis in</a>
@@ -32,7 +28,7 @@
             <div class="col-4 col-s-4 menu">
                 <div id="lasted_posts" class="text-uppercase font-weight-bold">Lasted Topic</div><br />
                 <div class="list-group">
-                    <a href="Postdetal.php" class="list-group-item list-group-item-action"><img src="images/user_avatar2.png" width="40" height="40" /> jkabfaksf</a>
+                    <a href="Postdetail.php" class="list-group-item list-group-item-action"><img src="images/user_avatar2.png" width="40" height="40" /> jkabfaksf</a>
 					<a href="Postdetail.php" class="list-group-item list-group-item-action"><img src="images/user_avatar3.png" width="40" height="40" /> Dapibus ac facilisis in</a>
 					<a href="Postdetail.php" class="list-group-item list-group-item-action"><img src="images/user_avatar2.png" width="40" height="40" /> jkabfaksf</a>
 					<a href="Postdetail.php" class="list-group-item list-group-item-action"><img src="images/user_avatar.png" width="40" height="40" /> Dapibus ac facilisis in</a>
@@ -43,7 +39,7 @@
             <div class="col-8 col-s-8 menu">
                 <div id="topic_name" class="text-uppercase font-weight-bold">Topic Name</div><br />
                 <div class="list-group">
-                    <a href="Postdetal.php" class="list-group-item list-group-item-action"><img src="images/user_avatar2.png" width="40" height="40" /> jkabfaksf</a>
+                    <a href="Postdetail.php" class="list-group-item list-group-item-action"><img src="images/user_avatar2.png" width="40" height="40" /> jkabfaksf</a>
 					<a href="Postdetail.php" class="list-group-item list-group-item-action"><img src="images/user_avatar3.png" width="40" height="40" /> Dapibus ac facilisis in</a>
 					<a href="Postdetail.php" class="list-group-item list-group-item-action"><img src="images/user_avatar2.png" width="40" height="40" /> jkabfaksf</a>
 					<a href="Postdetail.php" class="list-group-item list-group-item-action"><img src="images/user_avatar.png" width="40" height="40" /> Dapibus ac facilisis in</a>
@@ -51,10 +47,18 @@
 					<a href="Postdetail.php" class="list-group-item list-group-item-action"><img src="images/user_avatar3.png" width="40" height="40" /> Morbi leo risus</a>
                 </div>
             </div>
-
-        </div>
-
-        <?php include("./partials/footer.php")?>
-    </div>
+			</div>
+			</div>
+			<!-- FOOTER -->
+			  <?php include("./partials/footer.php") ?>
+			  <!-- Contact bar -->
+			  <ul class="contact-bar">
+				<li class="facebook">
+				  <a href="https://m.me" target="_blank" rel="nofollow"></a>
+				</li>
+				<li class="zalo">
+				  <a href="https://zalo.me" target="_blank" rel="nofollow"></a>
+				</li>
+			  </ul>
 </body>
 </html>
---
+++ student/about.php
@@ -0,0 +1,90 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("./partials/headlibs.php") ?>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Về Chúng Tôi</title>
+    <link rel="stylesheet" href="assets/css/forum.css" />
+    <link rel="stylesheet" href="./assets/css/index.css">
+</head>
+<body>
+    <?php include("./partials/header.php") ?>
+    <div class="container">
+        <div class="badge bg-primary text-wrap" style="width:1110px;height:50px;font-size:30px">ABOUT US</div>
+        <p style="text-align:justify;margin-top:20px">
+            Want to cultivate a broad vocabulary and boost pronunciation in a new language? LingoHut is the right place for you!
+            What is LingoHut?
+            LingoHut is a family-led project for language and culture that goes beyond borders.
+
+            It is a website that helps you find vocabulary, spelling, and pronunciation in carefully organized topics. A perfect resource for completely new, A1, and A2 learners to gain more confidence in listening and pronunciation. All recordings are from a native speaker of the language giving a natural understanding of pronunciation.
+
+
+            LingoHut as a tool is simple and powerful, especially for beginners of a language. Get used to the sounds and pronunciation, practice pronunciation alongside, and start gaining an understanding of spelling and reading through short phrases and words. Reinforce your knowledge by playing a variety of games at the end of each lesson.
+
+            It is 100% free-to-use and our gift to language learners.
+
+            It provides access to learn 50 languages, through a variety of games and lessons to learn.
+
+
+            Follow us and be part of the LingoHut community
+            Because LingoHut is not an average “learn a new language” site, it is a moving story of how learning a new language can transform you, and WE want to help you learn, grown, and speak with confidence.
+
+            LingoHut is a place for all.
+
+            Come learn to communicate in 1 or all 50 languages and read our blogs on our language journey.
+        </p>
+        <br>
+        <div class="badge bg-primary text-wrap" style="width:1110px;height:50px;font-size:30px">THE POWER WITHIN YOU</div>
+        <p style="text-align:justify;margin-top:32px">SUNSHINE is committed to maximizing and spreading the full potential of its students by constantly evoking and promoting their internal strengths and at the same time creating the best platform for young people perform to the best of their abilities. “The Power Within You” is meant to create and develop the potentials available within each person to achieve success with dedication, commitment and great effort</p>
+        <div class="badge bg-primary text-wrap" style="width:1110px;height:50px;font-size:30px;margin-top:20px">WHY SUNSHINE</div>
+        <p style="text-align:justify;margin-top:20px">With the mission of “Changing lives through education”, YOLA promises to optimize and unlock the full potential of its students through a solid foundation of education.</p><br>
+       <div class="row">
+        <div class="col-3 col-s-3">
+        <div class="card" style="width: auto;height:400px;">
+          <img class="card-img-top" src="images/about1.jpg" alt="Card image cap" width="auto" height="150px">
+          <div class="card-body">
+            <p class="card-text" style="text-align:center;">A long-term road map for children from 4,5 years old - 14 years old and above. From language, to knowledge, critical thinking, to summer camps experiences to develop linguistically, social skills and self-confidence.</p>
+          </div>
+        </div>
+        </div>
+        <div class="col-3 col-s-3">
+        <div class="card" style="width: auto;height:400px;">
+          <img class="card-img-top" src="images/about2.jpg" alt="Card image cap" width="auto" height="150px">
+          <div class="card-body">
+            <p class="card-text" style="text-align:center;">Guarantee of ultimate results for students through a variety of programs, intensive research on language development, problem-solving skills in both situations, thinking and tactics.</p>
+          </div>
+        </div>
+        </div>
+        <div class="col-3 col-s-3">
+        <div class="card" style="width: auto;height:400px;">
+          <img class="card-img-top" src="images/about3.jpg" alt="Card image cap" width="auto" height="150px">
+          <div class="card-body">
+            <p class="card-text" style="text-align:center;">The intensive academic program focuses on language development and critical thinking, based on international standards and designed to meet the needs of Vietnamese students.</p>
+          </div>
+        </div>
+        </div>
+        <div class="col-3 col-s-3">
+        <div class="card" style="width: auto;height:400px;">
+          <img class="card-img-top" src="images/about4.jpg" alt="Card image cap" width="auto" height="150px">
+          <div class="card-body">
+            <p class="card-text" style="text-align:center;">Pioneering in using technology to encourage self-study, full circle learning online-offline, and community building across Yola network of alumni who are successful globally.</p>
+          </div>
+        </div>
+        </div>
+        </div>
+    </div>
+    <!-- FOOTER -->
+    <?php include("./partials/footer.php") ?>
+    <!-- Contact bar -->
+    <ul class="contact-bar">
+        <li class="facebook">
+            <a href="https://m.me" target="_blank" rel="nofollow"></a>
+        </li>
+        <li class="zalo">
+            <a href="https://zalo.me" target="_blank" rel="nofollow"></a>
+        </li>
+    </ul>
+</body>
+</html>
