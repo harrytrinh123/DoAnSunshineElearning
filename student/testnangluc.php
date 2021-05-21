dc374b85f22d833fedf9eb6f510ea3b371004174
Jack Trinh <40319325+jacktrinh123@users.noreply.github.com>
2021-05-21 23:48:15 +0700

    Update testnangluc.php
    giao dien testnangluc

--- student/testnangluc.php
+++ student/testnangluc.php
@@ -2,57 +2,67 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <?php include("./partials/headlibs.php") ?>
-    <link rel="stylesheet" href="../student/assets/css/testnangluc.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <?php include("./partials/headlibs.php") ?>
+  <link rel="stylesheet" href="../student/assets/css/testnangluc.css">
 </head>
 
 <body>
-    <?php include("./partials/header.php") ?>
-    <div class="container">
-        <h1 class="text-center" style="padding: 10px; font-size: 35px;">BÀI KIỂM TRA ĐÁNH GIÁ NĂNG LỰC - Đề thi dựa theo chứng chỉ Cambrige - Số câu hỏi: 20</h1>
-        <div class="row">
-            <div class="col-md-10">
-                <div class="question ml-sm-5 pl-sm-5 pt-2">
-                    <div class="py-2 h5"><b>Q. which option best describes your job role?</b></div>
-                    <div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options">
-                        <label class="options">Small Business Owner or Employee <input type="radio" name="radio"> <span class="checkmark"></span> </label>
-                        <label class="options">Nonprofit Owner or Employee <input type="radio" name="radio"> <span class="checkmark"></span> </label>
-                        <label class="options">Journalist or Activist <input type="radio" name="radio"> <span class="checkmark"></span> </label>
-                        <label class="options">Other <input type="radio" name="radio"> <span class="checkmark"></span> </label>
-                    </div>
-                </div>
-                <div class="question ml-sm-5 pl-sm-5 pt-2">
-                    <div class="py-2 h5"><b>Q. which option best describes your job role?</b></div>
-                    <div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options">
-                        <label class="options">Small Business Owner or Employee <input type="radio" name="radio"> <span class="checkmark"></span> </label>
-                        <label class="options">Nonprofit Owner or Employee <input type="radio" name="radio"> <span class="checkmark"></span> </label>
-                        <label class="options">Journalist or Activist <input type="radio" name="radio"> <span class="checkmark"></span> </label>
-                        <label class="options">Other <input type="radio" name="radio"> <span class="checkmark"></span> </label>
-                    </div>
-                </div>
-                <div class="question ml-sm-5 pl-sm-5 pt-2">
-                    <div class="py-2 h5"><b>Q. which option best describes your job role?</b></div>
-                    <div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options">
-                        <label class="options">Small Business Owner or Employee <input type="radio" name="radio"> <span class="checkmark"></span> </label>
-                        <label class="options">Nonprofit Owner or Employee <input type="radio" name="radio"> <span class="checkmark"></span> </label>
-                        <label class="options">Journalist or Activist <input type="radio" name="radio"> <span class="checkmark"></span> </label>
-                        <label class="options">Other <input type="radio" name="radio"> <span class="checkmark"></span> </label>
-                    </div>
-                </div>
-                <div class="row" style="margin-top: 10px; margin-bottom: 10px;">
-                    <a class= "btn btn-primary " style="margin: 0 auto; width: 200px;" >Nộp bài</a>
-                </div>
-                
-            </div>
-            <div class="col-md-2">
-                <h2>Thời gian làm bài</h2>
-            </div>
+  <?php include("./partials/header.php") ?>
+  <div class="container">
+    <h1 class="text-center" style="padding: 10px; font-size: 35px;">BÀI KIỂM TRA ĐÁNH GIÁ NĂNG LỰC - Đề thi dựa theo chứng chỉ Cambrige - Số câu hỏi: 20</h1>
+    <div class="row">
+      <div class="col-md-10">
+        <div class="question ml-sm-5 pl-sm-5 pt-2">
+          <div class="py-2 h5"><b>Q. which option best describes your job role?</b></div>
+          <div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options">
+            <label class="options">Small Business Owner or Employee <input type="radio" name="radio"> <span class="checkmark"></span> </label>
+            <label class="options">Nonprofit Owner or Employee <input type="radio" name="radio"> <span class="checkmark"></span> </label>
+            <label class="options">Journalist or Activist <input type="radio" name="radio"> <span class="checkmark"></span> </label>
+            <label class="options">Other <input type="radio" name="radio"> <span class="checkmark"></span> </label>
+          </div>
         </div>
+        <div class="question ml-sm-5 pl-sm-5 pt-2">
+          <div class="py-2 h5"><b>Q. which option best describes your job role?</b></div>
+          <div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options">
+            <label class="options">Small Business Owner or Employee <input type="radio" name="radio"> <span class="checkmark"></span> </label>
+            <label class="options">Nonprofit Owner or Employee <input type="radio" name="radio"> <span class="checkmark"></span> </label>
+            <label class="options">Journalist or Activist <input type="radio" name="radio"> <span class="checkmark"></span> </label>
+            <label class="options">Other <input type="radio" name="radio"> <span class="checkmark"></span> </label>
+          </div>
+        </div>
+        <div class="question ml-sm-5 pl-sm-5 pt-2">
+          <div class="py-2 h5"><b>Q. which option best describes your job role?</b></div>
+          <div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options">
+            <label class="options">Small Business Owner or Employee <input type="radio" name="radio"> <span class="checkmark"></span> </label>
+            <label class="options">Nonprofit Owner or Employee <input type="radio" name="radio"> <span class="checkmark"></span> </label>
+            <label class="options">Journalist or Activist <input type="radio" name="radio"> <span class="checkmark"></span> </label>
+            <label class="options">Other <input type="radio" name="radio"> <span class="checkmark"></span> </label>
+          </div>
+        </div>
+        <div class="row" style="margin-top: 10px; margin-bottom: 10px;">
+          <a class="btn btn-primary " style="margin: 0 auto; width: 200px;">Nộp bài</a>
+        </div>
+
+      </div>
+      <div class="col-md-2">
+        <h2 style="font-size: 20px;">Thời gian làm bài</h2>
+      </div>
     </div>
+  </div>
+  <?php include("./partials/footer.php")?>
+  <!-- Contact bar -->
+  <ul class="contact-bar">
+    <li class="facebook">
+      <a href="https://m.me" target="_blank" rel="nofollow"></a>
+    </li>
+    <li class="zalo">
+      <a href="https://zalo.me" target="_blank" rel="nofollow"></a>
+    </li>
+  </ul>
 </body>
 
 </html>
